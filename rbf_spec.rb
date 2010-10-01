ac1d8aea6f10eab2ac161a0200fc04a269c6c9ae
Dezso Zoltan <dezso.zoltan@gmail.com>
2010-10-01 22:01:32 +0900

    Now with 75% more fizzbuzz power and minor refactoring in specs

--- rbf_spec.rb
+++ rbf_spec.rb
@@ -31,18 +31,20 @@ describe "RBC" do
   end
 
   it "should run hello world" do
-    oldstdout, $stdout = $stdout, StringIO.new
-    @rbfc.run('++++++++++[>+++++++>+++++++"+++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.')
-    $stdout.string.should == "Hello World!\n"
+    result = redirect_stdout do
+      @rbfc.run('++++++++++[>+++++++>+++++++"+++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.')
+    end
+    result.should == "Hello World!\n"
   end
 
   it "should ignore comments" do
-    oldstdout, $stdout = $stdout, StringIO.new
-    @rbfc.run('++++++++++Hello world with
-               added comments
-               enjoy # # [>+++++++>+++++++"+++>+++>+<<<<-]>++.>
-               +.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.')
-    $stdout.string.should == "Hello World!\n"
+    result = redirect_stdout do
+      @rbfc.run('++++++++++Hello world with
+                 added comments
+                 enjoy # # [>+++++++>+++++++"+++>+++>+<<<<-]>++.>
+                 +.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.')
+    end
+    result.should == "Hello World!\n"
   end
 
   it "should match brackets" do
@@ -59,5 +61,36 @@ describe "RBC" do
     @rbfc.buffer[@rbfc.ptr].should == 27
   end
 
+  it "should run fizzbuzz" do
+    result = redirect_stdout do
+      @rbfc.run(
+'>++++++++++[<++++++++++>-]>>>+++>+++++<<<<<[>>>>>>>+[<<<<<+<+>>>>>>-]<<<<<[>>>>>
++<<<<<-]++++++++++<[>-<-]>>+<[>[-]<[-]]>[>>>>[-]>+<<<<<-]>>>>>[<<<<<<+<+>>>>>>>-
+]<<<<<<[>>>>>>+<<<<<<-]++++++++++<[>-<-]>>+<[>[-]<[-]]>[>>>>>[-]>+<<<<<<-]>>>+<<
+-[<<+<+>>>-]<<<[>>>+<<<-]>>+<[>[-]<[-]]>[>+++<<+++++++[<++++++++++>-]<.>+++++[<+
+++++++>-]<.>+++[<+++++>-]<++..[-]>>>>>[-]<<<-]>>-[<<<+<+>>>>-]<<<[>>>+<<<-]>+<<[
+>>[-]<<[-]]>>[>>+++++<<<++++++++[<++++++++>-]<++.>++++++[<++++++++>-]<+++.+++++.
+.[-]>>>>>[-]<<<-]>>>[>>>++++++++++++++++++++++++++++++++++++++++++++++++.-------
+-----------------------------------------<++++++++++++++++++++++++++++++++++++++
+++++++++++.------------------------------------------------<++++++++++++++++++++
+++++++++++++++++++++++++++++.------------------------------------------------<-]
+<<<<++++[<+++++++++++>-]<.>+++[<---->-]<.[-]<-]'
+    )
+    end
+    result.should == '001, 002, Fizz, 004, Buzz, Fizz, 007, 008, Fizz, Buzz, 011, Fizz, 013, 014, FizzBuzz, ' +
+                     '016, 017, Fizz, 019, Buzz, Fizz, 022, 023, Fizz, Buzz, 026, Fizz, 028, 029, FizzBuzz, ' +
+                     '031, 032, Fizz, 034, Buzz, Fizz, 037, 038, Fizz, Buzz, 041, Fizz, 043, 044, FizzBuzz, ' +
+                     '046, 047, Fizz, 049, Buzz, Fizz, 052, 053, Fizz, Buzz, 056, Fizz, 058, 059, FizzBuzz, ' +
+                     '061, 062, Fizz, 064, Buzz, Fizz, 067, 068, Fizz, Buzz, 071, Fizz, 073, 074, FizzBuzz, ' +
+                     '076, 077, Fizz, 079, Buzz, Fizz, 082, 083, Fizz, Buzz, 086, Fizz, 088, 089, FizzBuzz, ' +
+                     '091, 092, Fizz, 094, Buzz, Fizz, 097, 098, Fizz, Buzz, '
+  end
 
+  def redirect_stdout
+    oldstdout, $stdout = $stdout, StringIO.new
+    yield
+    $stdout.string
+  ensure
+    $stdout = oldstdout
+  end
 end
